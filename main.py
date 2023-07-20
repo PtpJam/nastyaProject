23964f9395d33b13a5aeec8cd935e9a7295e74ed
PtpJam <ruslan.fateev.rf@gmail.com>
2023-07-20 09:43:54 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# from nastya import testFunc
+# from nastya import goodFunc
+
+
+# from nastya import testFunc, goodFunc
+
+# * get all
+# from nastya import *
+
+# from nastya import *
+
+import nastya
+
+if __name__ == '__main__':
+    nastya.testFunc()
+    nastya.goodFunc()
---
+++ nastya.py
@@ -0,0 +1,5 @@
+def goodFunc():
+    print("Hello")
+
+def testFunc():
+    print("Zoo")
